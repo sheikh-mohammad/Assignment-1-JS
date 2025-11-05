9387b89a057894cf5d4cb26d9df58270e6e5cfe5
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-05 16:29:19 +0500

    "Added student information variables and alert messages."

--- Chapter_2_Variables_for_Strings/Question_4/script.js
+++ Chapter_2_Variables_for_Strings/Question_4/script.js
@@ -1,1 +1,7 @@
- 
+var studentName = "John Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
