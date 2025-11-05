262ffe1a73d506ad720532cc86da45ef1c3e0fd0
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-05 17:03:02 +0500

    Added Question 4 in Chapter 3

--- Chapter_3_Variables_for_Numbers/Question_4/script.js
+++ Chapter_3_Variables_for_Numbers/Question_4/script.js
@@ -2,4 +2,4 @@ var visitorName = prompt("Enter Your Name:");
 var productTitle = prompt("Enter Product Title:");
 var quantity = prompt("How many products a visitor wants to order");
 
-document.write(`<b>${visitorName}</b> ordered <b>${quantity} ${productTitle}(s) on XYZ Clothing Store</b>`);
+document.write(`<b>${visitorName}</b> ordered <b>${quantity} ${productTitle}(s)</b> on XYZ Clothing Store`);
