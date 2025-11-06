1dcc6a1f70503749408b4c6018a7b652e0b9e943
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-06 16:13:27 +0500

    "Added JavaScript code to calculate and display arithmetic operations between two numbers."

---
+++ Chapter_5_Math_Expressions/Question_2/script.js
@@ -0,0 +1,13 @@
+var num1 = +prompt("Enter First Number");
+var num2 = +prompt("Enter Second Number");
+var sum = num1 + num2;
+var diff = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var remainder = num1 % num2;
+
+document.write(`Sum of ${num1} and ${num2} is ${sum} <br/>`);
+document.write(`Difference of ${num1} and ${num2} is ${diff} <br/>`);
+document.write(`Product of ${num1} and ${num2} is ${product}} <br/>`);
+document.write(`Quotient of ${num1} and ${num2} is ${quotient} <br/>`);
+document.write(`Remainder of ${num1} and ${num2} is ${remainder} <br/>`);
