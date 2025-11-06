9b75702fa0e3fefcffe248da69b006de78682371
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-06 07:00:27 +0500

    `Added code snippet for variable naming rules with legal and illegal examples`

---
+++ Chapter_4_Variable_Names_Legal_and_Illegal/Question_2/script.js
@@ -0,0 +1,13 @@
+// Legal Variables
+var legal;
+var legal1;
+var _legal;
+var $legal;
+var legalVariable;
+
+// Illegal Variables
+var illegal variable;
+var 1illegal;
+var illegal-1;
+var illegal@;
+var if;
