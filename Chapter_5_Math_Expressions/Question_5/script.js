d0c797ca41a79c90651160ea1e960573f20cbce8
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-13 21:13:14 +0500

    Added script file for Question 5 of Chapter 5

---
+++ Chapter_5_Math_Expressions/Question_5/script.js
@@ -0,0 +1,13 @@
+var num = +prompt("Enter Any Number:");
+
+document.write(`Table of ${num} <br>`);
+document.write(`${num}x1=${num * 1} <br>`);
+document.write(`${num}x2=${num * 2} <br>`);
+document.write(`${num}x3=${num * 3} <br>`);
+document.write(`${num}x4=${num * 4} <br>`);
+document.write(`${num}x5=${num * 5} <br>`);
+document.write(`${num}x6=${num * 6} <br>`);
+document.write(`${num}x7=${num * 7} <br>`);
+document.write(`${num}x8=${num * 8} <br>`);
+document.write(`${num}x9=${num * 9} <br>`);
+document.write(`${num}x10=${num * 10}`);
