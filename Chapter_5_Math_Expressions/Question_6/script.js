6f5e6c0f94977fb50064558355ccd1d5dd5bf414
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-16 08:42:32 +0500

    Add temperature conversion script for Celsius and Fahrenheit

---
+++ Chapter_5_Math_Expressions/Question_6/script.js
@@ -0,0 +1,5 @@
+var celsius = 25;
+document.write(`${celsius}<sup>0</sup>C is ${(celsius * 9 / 5) + 32}<sup>0</sup>F <br />`);
+
+var fahrenheit = 70;
+document.write(`${fahrenheit}<sup>0</sup>F is ${(fahrenheit - 32) * 5 / 9}<sup>0</sup>C <br />`);
