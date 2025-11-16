27a28d54ba8f0651daed298fb1b84658fc73d8eb
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-16 15:28:46 +0500

    Add JavaScript solution for Chapter 5 Question 11 - Age Calculator

---
+++ Chapter_5_Math_Expressions/Question_11/script.js
@@ -0,0 +1,9 @@
+var currentYear = 2025;
+var birthYear = 2011;
+var age = currentYear - birthYear;
+
+document.write("<h1> Age Calculator </h1>");
+document.write("<br/>");
+document.write(`Current Year: ${currentYear} <br/>`);
+document.write(`Birth Year: ${birthYear} <br/>`);
+document.write(`Your Age: ${age}`);
