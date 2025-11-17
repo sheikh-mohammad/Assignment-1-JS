12b1b8bbd873da1f0911b0f5ae0877fe81ea3536
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-17 22:53:22 +0500

    Add solution for Chapter 5 Math Expressions Question 12 - The Geometrizer

---
+++ Chapter_5_Math_Expressions/Question_12/script.js
@@ -0,0 +1,9 @@
+var radius = 20;
+var cicumference = 2 * 3.142 * radius;
+var area = 3.142 * radius ** 2;
+
+document.write("<h1> The Geometrizer </h1>");
+document.write("<br />");
+document.write(`Radius of a circle: ${radius} <br />`);
+document.write(`The circumference is: ${cicumference} <br />`);
+document.write(`The area is: ${area}`);
