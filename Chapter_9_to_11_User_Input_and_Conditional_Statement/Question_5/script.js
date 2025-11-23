477337ea9536052820ea98177607a0bd85733139
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-23 17:23:40 +0500

    Update script.js

---
+++ Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/script.js
@@ -0,0 +1,7 @@
+var a = 4;
+if (++a === 5) {
+    alert("given condition for variable a is true");
+}
+
+// alert("given condition for variable a is true");
+// This script runs because ++a is pre-increment and pre-increment means first update and then use it as well.
