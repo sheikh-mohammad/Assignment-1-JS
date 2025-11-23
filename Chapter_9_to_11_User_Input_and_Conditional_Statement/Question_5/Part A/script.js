ccaff7bb4ef96888c2ce9e8d037a0f1a742a8956
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-23 17:30:10 +0500

    Refactor script.js for clarity on pre-increment
    Updated comments to clarify the behavior of pre-increment.

--- Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part A/script.js
+++ Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part A/script.js
@@ -3,5 +3,7 @@ if (++a === 5) {
     alert("given condition for variable a is true");
 }
 
+// Output:
 // alert("given condition for variable a is true");
-// This script runs because ++a is pre-increment and pre-increment means first update and then use it as well.
+
+// Reason: This script produces output because ++a is pre-increment and pre-increment means first update and then use it as well. That's why the condition becomes true.
