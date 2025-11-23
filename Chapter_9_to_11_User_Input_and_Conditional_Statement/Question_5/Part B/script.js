aadeadfe20fdfff87951c4b12dcf1e65f7b4adb2
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-23 17:28:34 +0500

    Update script.js

--- Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part B/script.js
+++ Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part B/script.js
@@ -2,3 +2,8 @@ var b = 82;
 if (b++ === 83) {
     alert("given condition for variable b is true");
 }
+
+// Output:
+// 
+
+// Reason: This script produces np output because b++ is post-increment and post-increment means first use and then update. That's why the condition becomes false.
