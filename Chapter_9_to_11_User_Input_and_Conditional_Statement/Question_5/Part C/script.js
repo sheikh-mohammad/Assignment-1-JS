eacd4d2a7298cfb318686bf9e5a93f293236ee8f
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-23 17:41:03 +0500

    Refine output comments and reasoning in script.js
    Updated comments to clarify output reasoning for conditions.

--- Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part C/script.js
+++ Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part C/script.js
@@ -16,7 +16,22 @@ if (c === 14) {
     alert("condition 4 is true");
 }
 
-// Output:
+// Output 1:
 //
 
-// Reason: This script produces np output because b++ is post-increment and post-increment means first use and then update. That's why the condition becomes false.
+// Reason: The first code block produces no output because b++ is post-increment and post-increment means first use and then update. That's why the condition becomes false.
+
+// Output 2:
+// condition 2 is true
+
+// Reason: The second code block produces output because b is now updated by post-increment and post-increment means first use and then update. That's why the condition becomes true.
+
+// Output 3:
+//
+
+// Reason: The third code block produces no output because ++b is pre-increment and pre-increment means first update and then use it as well. That's why the condition becomes false. Because it was updated.
+
+// Output 4:
+// condition 4 is true
+
+// Reason: This script produces output because b now updated by pre-increment and pre-increment means first update and then use it as well. That's why the condition becomes true. Because it was updated.
