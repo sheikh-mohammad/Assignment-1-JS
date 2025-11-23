55e0b3a59100a4c70360ca7e2698485a2af86f3f
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-23 17:35:38 +0500

    Fix increment operator usage in condition checks

--- Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part C/script.js
+++ Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part C/script.js
@@ -1,13 +1,17 @@
 var c = 12;
+
 if (c++ === 13) {
     alert("condition 1 is true");
 }
+
 if (c === 13) {
     alert("condition 2 is true");
 }
+
 if (++c < 14) {
     alert("condition 3 is true");
 }
+
 if (c === 14) {
     alert("condition 4 is true");
 }
