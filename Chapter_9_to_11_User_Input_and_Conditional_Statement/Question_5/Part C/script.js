8aabbf0bf067f33c4ec3102092b5e69ae25936c4
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-23 17:35:20 +0500

    Refactor conditional statements in script.js

--- Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part C/script.js
+++ Chapter_9_to_11_User_Input_and_Conditional_Statement/Question_5/Part C/script.js
@@ -2,15 +2,15 @@ var c = 12;
 if (c++ === 13) {
     alert("condition 1 is true");
 }
-if (c === 13){
+if (c === 13) {
     alert("condition 2 is true");
-    }
-    if (++c < 14){
-        alert("condition 3 is true");
-        }
-        if(c === 14){
-            alert("condition 4 is true");
-            }
+}
+if (++c < 14) {
+    alert("condition 3 is true");
+}
+if (c === 14) {
+    alert("condition 4 is true");
+}
 
 // Output:
 //
